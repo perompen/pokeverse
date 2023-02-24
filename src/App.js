8884e63891036189ede609b3de21168400cc37e1
Perom Peter Pen <officialpeterp@yahoo.com>
2023-02-24 12:47:48 -0800

    updates App.js & PokemonCard.js / renders pokemon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Container from 'react-bootstrap/Container';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
+
+  function handleChange(ev) {
+    // store input value
+    const val = ev.target.value;
+    // create regex to match the input value
+    const regex = new RegExp(val, "gi");
+    // filter pokemon matches
+    const filteredPokeTemp = pokemon.filter((onePoke) => {
+      return onePoke.name.match(regex);
+    });
+    setFilteredPokemon(filteredPokeTemp);
+  }
+
+  useEffect(() => {
+    fetch(pokeApi).then((response) => response.json()).then((data) => {
+      // console.log(data.results)
+      setPokemon(data.results);
+      setFilteredPokemon(data.results);
+    });
+  }, []);
 
   return (
     <div data-testid="app">
       <Navigation />
 
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      {/* <h1>Pokemon should appear here</h1> */}
+      <InputGroup>
+        <FormControl placeholder="search for pokemon" onChange={handleChange}></FormControl>
+      </InputGroup>
+      <Container id="pokemonCards">
+        {filteredPokemon.map((singlePoke, idx) => {
+          return <PokemonCard key={idx} name={singlePoke.name} url={singlePoke.url} />;
+        })}
+      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
 
 function PokemonCard({ url, name }) {
+  const [details, setDetails] = useState({
+    sprites: [],
+    abilities: [],
+  });
+
+  fetch(url).then(response => response.json()).then(data => {
+    setDetails({
+      sprites: data.sprites,
+      abilities: data.abilities,
+    });
+  });
 
   return (
     <div>
-        pokemon card
+      pokemon card
+      {/* <h1>pokemon card</h1> */}
+      <Card id="pokemonCard">
+        <Card.Img src={details.sprites.front_default} />
+        <Card.Title>{name}</Card.Title>
+        <Card.Text as="div">
+          <ul>
+            {details.abilities.map((a) => {
+              return <li>{a.ability.name}</li>;
+            })}
+          </ul>
+        </Card.Text>
+      </Card>
     </div>
   );
 }
